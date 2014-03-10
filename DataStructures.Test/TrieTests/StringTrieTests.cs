652aceb71337c2a3a2233fd2b04e18e76c145139
Matt Nedrich <mattnedrich@gmail.com>
2014-03-09 23:18:38 -0400

    Initial commit of DataStructures project
    Initial commit
    Added StringTrie

---
+++ DataStructures.Test/TrieTests/StringTrieTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MattNedrich.DataStructures.Trie;
+
+namespace DataStructures.Test.TrieTests
+{
+    [TestFixture]
+    public class StringTrieTests
+    {
+        private StringTrie trie = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            trie = new StringTrie();
+        }
+
+        [Test]
+        public void Can_Add_String_To_Trie()
+        {
+            trie.Add("test");
+            Assert.True(trie.Contains("test"));
+        }
+
+        [Test]
+        public void Integration_Tests()
+        {
+            trie.Add("dog");
+            trie.Add("tree");
+            trie.Add("frog");
+            trie.Add("house");
+            trie.Add("computer");
+
+            Assert.True(trie.Contains("dog"));
+            Assert.True(trie.Contains("tree"));
+            Assert.True(trie.Contains("frog"));
+            Assert.True(trie.Contains("house"));
+            Assert.True(trie.Contains("computer"));
+            Assert.False(trie.Contains("dogs"));
+            Assert.False(trie.Contains("monday"));
+        }
+    }
+}
---
+++ DataStructures/Trie/StringTrie.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MattNedrich.DataStructures.Trie
+{
+    public class StringTrie
+    {
+        TrieLevel root;
+        int baseCode = (int)'a';
+
+        public StringTrie()
+        {
+            root = new TrieLevel();
+        }
+
+        public void Add(string input)
+        {
+            TrieLevel current = root;
+            char[] chars = input.ToLower().ToArray();
+            for(int i=0; i<chars.Length; i++)
+            {
+                char c = chars[i];
+                int index = (int)c - baseCode;
+                if (i == chars.Length - 1)
+                    current.Entries[index].Set = true;
+                else
+                {
+                    // continue down the tree
+                    if (current.Entries[index].Next == null)
+                        current.Entries[index].Next = new TrieLevel();
+                    current = current.Entries[index].Next;
+                }
+            }
+        }
+
+        public bool Contains(string input)
+        {
+            bool result = false;
+            TrieLevel current = root;
+            char[] chars = input.ToLower().ToArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                char c = chars[i];
+                int index = (int)c - baseCode;
+                if (i == chars.Length - 1)
+                {
+                    result = current.Entries[index].Set;
+                    break;
+                }
+                else
+                {
+                    // continue down the tree
+                    if (current.Entries[index].Next == null)
+                        return false;
+                    current = current.Entries[index].Next;
+                }
+            }
+            return result;
+        }
+
+        private class TrieLevel
+        {
+            public TrieEntry[] Entries { get; private set; }
+
+            public TrieLevel()
+            {
+                Entries = new TrieEntry[32];
+                for (int i = 0; i < Entries.Length; i++)
+                    Entries[i] = new TrieEntry();
+            }
+        }
+
+        private class TrieEntry
+        {
+            public bool Set { get; set; }
+            public TrieLevel Next { get; set; }
+            public TrieEntry()
+            {
+                Set = false;
+                Next = null;
+            }
+        }
+    }
+
+
+
+}
